ab92ba96defa60b9fda31b68c349bda66b37167d
chenguolin <chenguolin@tencent.com>
2025-12-17 15:17:37 +0800

    diffusion.model name_lr_mult

--- src/options.py
+++ src/options.py
@@ -179,17 +179,17 @@ opt_dict: Dict[str, Options] = {}
 # Wan2.1-T2V-1.3B
 opt_dict["wan2.1_t2v_1.3b"] = Options(
     input_plucker=True,
-    exclude_name_lr_mult="plucker,da3_adapter",
+    name_lr_mult="diffusion.model",
 )
 opt_dict["wan2.1_t2v_1.3b_i2v"] = Options(
     first_latent_cond=True,
     input_plucker=True,
-    exclude_name_lr_mult="plucker,da3_adapter",
+    name_lr_mult="diffusion.model",
 )
 opt_dict["wan2.1_t2v_1.3b_i2v_causal"] = Options(
     first_latent_cond=True,
     input_plucker=True,
-    exclude_name_lr_mult="plucker,da3_adapter",
+    name_lr_mult="diffusion.model",
     #
     is_causal=True,
     use_teacher_forcing=True,
