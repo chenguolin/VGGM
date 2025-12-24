643d990ee4f6624eb1f46a4a4ed348cfa5e11760
chenguolin <chenguolin@tencent.com>
2025-12-24 17:37:17 +0800

    taehv

--- src/models/networks/__init__.py
+++ src/models/networks/__init__.py
@@ -1,4 +1,5 @@
 from src.models.networks.conv import FeatureEmbed
+from src.models.networks.taehv import TAEHV
 from src.models.networks.wan_wrapper import (
     WanCLIPEncoderWrapper,
     WanTextEncoderWrapper,
