ae809994f5bb1a252f781e823111c029ac46b63c
chenguolin <chenguolin@tencent.com>
2026-01-19 02:42:59 +0800

    minor

--- src/options.py
+++ src/options.py
@@ -323,7 +323,7 @@ opt_dict["wan2.1_t2v_1.3b_dmd"] = Options(
     #
     load_da3=False,
     da3_interactive=True,
-    da3_weight_type="uniform",
+    da3_weight_type="inverse_timestep",
     da3_down_ratio=1,
     da3_loss_in_sf=False,
     # only_train_resdit=True,
