12d3592218a047f186167f2444acb451fd14c280
chenguolin <chenguolin@tencent.com>
2026-02-05 22:20:33 +0800

    da3_wrapper

--- src/models/networks/__init__.py
+++ src/models/networks/__init__.py
@@ -1,5 +1,6 @@
 from .taehv import TAEHV
 from .decoder_wrapper import VAEDecoderWrapper
+from .da3_wrapper import DA3Wrapper
 from .wan_wrapper import (
     WanCLIPEncoderWrapper,
     WanTextEncoderWrapper,
